223cac95603551f7294a586e1857b0852c61bf0f
Dorokhov-Vadim <dorokhov.vadim999gmail.com>
2021-11-11 10:16:47 +0300

    add .gitignore

--- strategies/correlation.py
+++ strategies/correlation.py
@@ -1,3 +1,5 @@
+import math
+
 from mtester.base_strategies import BaseCandleStrategy
 from typing import List, Dict
 from mtester.instruments import Instrument
@@ -13,40 +15,19 @@ from datetime import datetime
 class Correlation(BaseCandleStrategy):
 
     def on_candle_close(self, closed_candles: Dict[Instrument, List[Candle]], current_candle: Dict[Instrument, CurCandle]):
+        # Init data structure
+        rts_count = 1
+        si_max_count = 14
+        si_act_val = 80
         data_si = []
-        for candle in closed_candles[self.pos_by_ticker('si').instrument]:
-            data_si.append(candle.close)
-        cur_price_si = current_candle[self.pos_by_ticker('si').instrument].price
         data_rts = []
-        for candle in closed_candles[self.pos_by_ticker('rts').instrument]:
-            data_rts.append(candle.close)
-        cur_price_rts = current_candle[self.pos_by_ticker('rts').instrument].price
+        rts_instrument = self.pos_by_ticker('rts').instrument
+        si_instrument = self.pos_by_ticker('si').instrument
 
 
-        nd_si = []
-        nd_rts = []
-        for x in data_si:
-            nd_si.append([x])
-        for x in data_rts:
-            nd_rts.append([x])
-
-        model = LinearRegression(fit_intercept=True)
-        model.fit(nd_rts, nd_si)
 
 
-        # Predict
-        # nd_si_model = model.predict(nd_rts)
-        si_predict = model.predict([[cur_price_rts]])
-        print(si_predict[0][0])
-        self.trade.stat.add_indicator(self.pos_by_ticker('si').instrument, 'si_model', self.cur_date,
-                                      self.cur_time, si_predict[0][0], 'rs')
-        self.trade.stat.add_indicator(self.pos_by_ticker('si').instrument, 'si_fact', self.cur_date,
-                                      self.cur_time, cur_price_si, 'gs')
-        # plt.figure()
-        # plt.scatter(nd_rts, nd_si, c="k", label="fact data")
-        # plt.plot(nd_rts, nd_si_model, c="g", label="model_data", linewidth=2)
-        # plt.show()
-        # exit()
+
 
 
 
--- testing.py
+++ testing.py
@@ -9,9 +9,16 @@ class BaseTest:
 
 
 class CandleTest(BaseTest):
-    def __init__(self, strategy: BaseCandleStrategy, data_provider: Iterable):
+    def __init__(self, strategy: BaseCandleStrategy, data_provider: Iterable, from_num_candle=0, to_num_candle=0, show_balance_changing=False):
         self.strategy: BaseCandleStrategy = strategy
         self.data_provider = data_provider
+        self.from_num_candle = from_num_candle
+        self.to_num_candle = to_num_candle
+        self.show_balance_changing = show_balance_changing
+
+    def set_interval(self, from_num_candle=0, to_num_candle=0):
+        self.from_num_candle = from_num_candle
+        self.to_num_candle = to_num_candle
 
     def show_trade_stat(self):
         self.strategy.trade.stat.show_trade_stat()
@@ -19,7 +26,17 @@ class CandleTest(BaseTest):
     def show_instrument(self, instrument):
         self.strategy.trade.stat.show_instrument(instrument)
 
+    def is_allow_interval(self, candle_num):
+        res = False
+        if self.from_num_candle <= candle_num <= self.to_num_candle \
+                or ((self.from_num_candle == 0) and candle_num <= self.to_num_candle) \
+                or ((self.to_num_candle == 0) and candle_num >= self.from_num_candle) \
+                or ((self.from_num_candle == 0) and (self.to_num_candle == 0)):
+            res = True
+        return res
+
     def run(self):
+        candle_count = 0
         if self.strategy is None:
             raise Exception('strategy is None')
         if self.data_provider is None:
@@ -27,66 +44,72 @@ class CandleTest(BaseTest):
         print('Market testing is running...')
         closed_candles = []
         prev_close = None
+        prev_balance = 0
 
         for data_batch in self.data_provider:
-            if not isinstance(data_batch, list):
-                data_batch = [data_batch]
-            if len(closed_candles) > 0:
-                for position in self.strategy.trade.positions:
-                    for def_order in position.deferred_orders:
-                        for candle in closed_candles:
-                            if candle.instrument is position.instrument:
-                                if prev_close is None:
-                                    prev_close = candle.close
-                                if (prev_close > candle.high and candle.low < def_order.price < prev_close)\
-                                or (prev_close < candle.low and prev_close < def_order.price < candle.high)\
-                                or (candle.low <= prev_close <= candle.high
-                                    and candle.low < def_order.price < candle.high):
-                                    if def_order.oper == 'B':
-                                        self.strategy.trade.buy(position.instrument, def_order.price, def_order.count,
-                                                                def_order.order_type, candle.date, candle.time)
-                                        self.strategy.trade.stat.add_buy(position.instrument, candle.date,
-                                                                         candle.time, def_order.price, def_order.count)
-                                    if def_order.oper == 'S':
-                                        self.strategy.trade.sell(position.instrument, def_order.price, def_order.count,
-                                                                 def_order.order_type, candle.date, candle.time)
-                                        self.strategy.trade.stat.add_sell(position.instrument, candle.date,
-                                                                          candle.time, def_order.price, def_order.count)
-                                    position.deferred_orders.remove(def_order)
-                                prev_close = candle.close
-                self.strategy.trade.stat.balance_hist.append(self.strategy.trade.balance)
-                # stat collection
-
-                for candle in closed_candles:
-                    lose = 0
-                    profit = 0
+            candle_count = candle_count + 1
+            if self.is_allow_interval(candle_count):
+                if not isinstance(data_batch, list):
+                    data_batch = [data_batch]
+                if len(closed_candles) > 0:
                     for position in self.strategy.trade.positions:
-                        if position.instrument is candle.instrument:
-                            coef = position.instrument.step_price/position.instrument.step
-                            if position.mean_price > candle.low and position.count > 0:
-                                lose = (position.mean_price - candle.low) * position.count * coef
-                            if position.mean_price < candle.high and position.count < 0:
-                                lose = (candle.high - position.mean_price) * abs(position.count) * coef
-                            if position.mean_price < candle.high and position.count > 0:
-                                profit = (candle.high - position.mean_price) * position.count * coef
-                            if position.mean_price > candle.low and position.count < 0:
-                                profit = (position.mean_price - candle.low) * abs(position.count) * coef
-                    self.strategy.trade.stat.add_candle(candle.instrument, candle.date,
-                                                        candle.time, candle.open, candle.low,
-                                                        candle.high, candle.close,
-                                                        lose, profit)
-                    #print(candle.date + ' '+candle.time+' '+str(self.strategy.trade.balance) + ' '+str(lose) +' / ' +str(profit))
+                        for def_order in position.deferred_orders:
+                            for candle in closed_candles:
+                                if candle.instrument is position.instrument:
+                                    if prev_close is None:
+                                        prev_close = candle.close
+                                    if (prev_close > candle.high and candle.low < def_order.price < prev_close)\
+                                    or (prev_close < candle.low and prev_close < def_order.price < candle.high)\
+                                    or (candle.low <= prev_close <= candle.high
+                                        and candle.low < def_order.price < candle.high):
+                                        if def_order.oper == 'B':
+                                            self.strategy.trade.buy(position.instrument, def_order.price, def_order.count,
+                                                                    def_order.order_type, candle.date, candle.time)
+                                            self.strategy.trade.stat.add_buy(position.instrument, candle.date,
+                                                                             candle.time, def_order.price, def_order.count)
+                                        if def_order.oper == 'S':
+                                            self.strategy.trade.sell(position.instrument, def_order.price, def_order.count,
+                                                                     def_order.order_type, candle.date, candle.time)
+                                            self.strategy.trade.stat.add_sell(position.instrument, candle.date,
+                                                                              candle.time, def_order.price, def_order.count)
+                                        position.deferred_orders.remove(def_order)
+                                    prev_close = candle.close
+                    self.strategy.trade.stat.balance_hist.append(self.strategy.trade.balance)
+                    # stat collection
 
-                cur_data_batch = dict()
-                for now_candle in data_batch:
-                    cur_candle = CurCandle(now_candle.instrument)
-                    cur_candle.date = now_candle.date
-                    cur_candle.time = now_candle.time
-                    cur_candle.price = now_candle.open
-                    cur_data_batch[now_candle.instrument] = cur_candle
+                    for candle in closed_candles:
+                        lose = 0
+                        profit = 0
+                        for position in self.strategy.trade.positions:
+                            if position.instrument is candle.instrument:
+                                coef = position.instrument.step_price/position.instrument.step
+                                if position.mean_price > candle.low and position.count > 0:
+                                    lose = (position.mean_price - candle.low) * position.count * coef
+                                if position.mean_price < candle.high and position.count < 0:
+                                    lose = (candle.high - position.mean_price) * abs(position.count) * coef
+                                if position.mean_price < candle.high and position.count > 0:
+                                    profit = (candle.high - position.mean_price) * position.count * coef
+                                if position.mean_price > candle.low and position.count < 0:
+                                    profit = (position.mean_price - candle.low) * abs(position.count) * coef
+                        self.strategy.trade.stat.add_candle(candle.instrument, candle.date,
+                                                            candle.time, candle.open, candle.low,
+                                                            candle.high, candle.close,
+                                                            lose, profit)
 
-                self.strategy.cur_date = data_batch[0].date
-                self.strategy.cur_time = data_batch[0].time
-                self.strategy.receive_data(closed_candles, cur_data_batch)
-            closed_candles = data_batch
+                    cur_data_batch = dict()
+                    for now_candle in data_batch:
+                        cur_candle = CurCandle(now_candle.instrument)
+                        cur_candle.date = now_candle.date
+                        cur_candle.time = now_candle.time
+                        cur_candle.price = now_candle.open
+                        cur_data_batch[now_candle.instrument] = cur_candle
+
+                    self.strategy.cur_date = data_batch[0].date
+                    self.strategy.cur_time = data_batch[0].time
+                    self.strategy.receive_data(closed_candles, cur_data_batch)
+                closed_candles = data_batch
+
+                if prev_balance != self.strategy.trade.balance:
+                    print('Balance = ' + str(self.strategy.trade.balance))
+                    prev_balance = self.strategy.trade.balance
         print('Market testing is done.')
